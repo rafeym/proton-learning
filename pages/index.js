0dbc418346ec7b3d3c1e6be70f205ca3426a4e8b
rafeym <mohammadrafey0@gmail.com>
2020-02-14 23:10:18 -0500

    DEVELOP: Web applications index page for rendering created components

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import LandingPage from '../components/Index/LandingPage'
+import OurServices from '../components/Index/OurServices'
+import Banner from '../components/Banner/Banner'
+
+const Index = () => {
+  return (
+    <>
+      <LandingPage />
+      <OurServices />
+      <Banner text='At Proton we ensure success, and excellence for all our students' />
+    </>
+  )
+}
+
+export default Index
