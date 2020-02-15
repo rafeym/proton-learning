cf1fdd5b73f1ffbe5e29e120c4f860fe7f5781d6
rafeym <mohammadrafey0@gmail.com>
2020-02-14 23:06:43 -0500

    DEVELOP: Services list  component complete

---
+++ components/OurServices/Services.js
@@ -0,0 +1,59 @@
+const Services = () => {
+  return (
+    <>
+      <nav class='level is-mobile'>
+        <div class='level-item has-text-centered'>
+          <figure class='image is-128x128'>
+            <img
+              class='is-rounded'
+              src='https://gradepowerlearning.com/wp-content/uploads/2015/10/icon-memory-retention.png'
+            />
+          </figure>
+        </div>
+        <div class='level-item has-text-centered'>
+          <figure class='image is-128x128'>
+            <img
+              class='is-rounded'
+              src='https://gradepowerlearning.com/wp-content/uploads/2015/10/icon-study-skills.png'
+            />
+          </figure>
+        </div>
+        <div class='level-item has-text-centered'>
+          <figure class='image is-128x128'>
+            <img
+              class='is-rounded'
+              src='https://gradepowerlearning.com/wp-content/uploads/2015/10/icon-academic-confidence.png'
+            />
+          </figure>
+        </div>
+        <div class='level-item has-text-centered'>
+          <figure class='image is-128x128'>
+            <img
+              class='is-rounded'
+              src='https://www.oxfordlearning.com/wp-content/uploads/2015/10/icon-geometric.png'
+            />
+          </figure>
+        </div>
+      </nav>
+      <h3 id='serviceTitle'>Proton Learning has all the answers</h3>
+      <p className='serviceParagraph'>
+        We make sure that our students get personalized attention in our small
+        group of classes.
+      </p>
+      <p className='serviceParagraph'>
+        To instill confidence and good study skills in students and develop
+        leadership like qualities.
+      </p>
+      <p className='serviceParagraph'>
+        We provide our students with appropriate materials to practice and
+        enhance their academic skills.
+      </p>
+      <p className='serviceParagraph'></p>
+      <button className='button' id='service-button'>
+        Contact Us
+      </button>
+    </>
+  )
+}
+
+export default Services
