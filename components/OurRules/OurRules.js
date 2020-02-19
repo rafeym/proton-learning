28c61c67d58b1a4d80e01688fecb7e46fac071ee
rafeym <mohammadrafey0@gmail.com>
2020-02-18 20:24:21 -0500

    DEVELOP: Rules segment created

---
+++ components/OurRules/OurRules.js
@@ -0,0 +1,25 @@
+const OurRules = () => {
+  return (
+    <div className='hero is-medium' id='heroRules'>
+      <div className='hero-body'>
+        <div className='container has-text-centered' id='cuntCont'>
+          <nav class='level'>
+            <div class='level-item has-text-centered'>
+              <div>
+                <p className='ruleNumber'>1</p>
+                <p className='ruleTitle'>Success</p>
+                <p class='ruleParagraph'>
+                  We at proton learning ensure student sucess. With our
+                  interative, engaging classroom envirments it instills that our
+                  students succeeed in whatever subject they are taking.
+                </p>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OurRules
